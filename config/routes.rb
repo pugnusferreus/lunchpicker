3f655c546515c350d17346e65d7477238c1cfbbb
Jason Weathered <git@jason.backtick.org>
2009-10-02 13:01:55 +1000

    Remove duplicate venues route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :venues
 
-  map.resources :venues
-
   map.resources :locations
 
   # The priority is based upon order of creation: first created -> highest priority.
