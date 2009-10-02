5d97148e39233f3d3e8994e2df1d6d63e58b76a0
Jason Weathered <git@jason.backtick.org>
2009-10-02 13:16:59 +1000

    Cleanup search model.

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,25 +1,27 @@
-class Search 
+class Search
   include Validatable
-
+  
   validates_presence_of :location_id
 
   attr_accessor :location_id, :location_name, :venue_name, :venue_address, :comments
-
   
   def find_venue(loc_id)
      @location_id = loc_id
-     if (self.valid?) then 
-       @venue = Venue.first(:order => 'random()',:conditions => ["location_id = ?", @location_id])
      
-
-       if(!@venue.nil?) then
-         @location = Location.find_by_location_id(@location_id)
-         @location_name = @location.name
-         @venue_name = @venue.name
-         @venue_address = @venue.address
-         @comments = @venue.comments
-         @location_id = loc_id
-       end
+    if valid?
+       
+      @venue = Venue.first :order => 'random()', :conditions => ["location_id = ?", @location_id]
+      
+      if not @venue.nil?
+        @location = Location.find @location_id
+        @location_name = @location.name
+        @venue_name = @venue.name
+        @venue_address = @venue.address
+        @comments = @venue.comments
+      end
+      
     end
+    
   end
+  
 end
