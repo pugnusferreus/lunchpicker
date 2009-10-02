fb864b43afb73dd9f0e50c591be3412b179771a7
Jason Weathered <git@jason.backtick.org>
2009-10-02 12:56:34 +1000

    Add validatable gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,3 @@
-require "validatable"
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
@@ -23,6 +22,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'validatable'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
