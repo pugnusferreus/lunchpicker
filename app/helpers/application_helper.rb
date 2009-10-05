adf5989e862b160bd829016988899547f811183d
Benson Lim <benson@spawncamp.net>
2009-10-05 12:03:04 +0800

    needs jason's help on heredocs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,22 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def error_messages_for_attribute(object, attribute, field_label)
+    if object.errors.on(attribute)
+      html = '<small class="errors"><ul>'
+      object.errors.on(attribute).each do |message|
+        html += "<li>" + field_label + " " + message + "</li>"
+      end
+      html += "</ul></small>"
+    end
+  end
+
+  def generate_map_canvas(location)
+    if(!location.nil?) then
+      return <<EOF
+      lawl
+      EOF
+    end
+  end
+
 end
 
-def error_messages_for_attribute(object, attribute, field_label)
-  if object.errors.on(attribute)
-    html = '<small class="errors"><ul>'
-    object.errors.on(attribute).each do |message|
-      html += "<li>" + field_label + " " + message + "</li>"
-    end
-    html += "</ul></small>"
-  end
-end
