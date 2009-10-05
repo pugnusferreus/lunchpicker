7c1ae1ae94409e261bcd62b6eef8c4dfd696104d
Jason Weathered <git@jason.backtick.org>
2009-10-05 21:32:57 +1000

    Refactor Maps javascript

--- public/javascripts/google_map.js
+++ public/javascripts/google_map.js
@@ -1,35 +1,49 @@
-var map = null;
+var maps = new Object();
 var geocoder = null;
 
-function initMap(id) 
+function getMap(id)
 {
-    if (GBrowserIsCompatible()) 
-    {
-        map = new GMap2(document.getElementById(id));
-        geocoder = new GClientGeocoder();
-        var customUI = map.getDefaultUI();
-        customUI.maptypes.hybrid = false;
-        map.setUI(customUI);
-    }
-      
+	
+	if (GBrowserIsCompatible() && !maps[id])
+	{
+		
+		var map = new GMap2(document.getElementById(id));
+		
+		var customUI = map.getDefaultUI();
+		customUI.maptypes.hybrid = false;
+		map.setUI(customUI);
+		
+		maps[id] = map;
+		
+	}
+	
+	return maps[id];
+	
+}
+
+function showPoint(id, point, text)
+{
+	var map = getMap(id);
+	map.setCenter(point, 13);
+	var marker = new GMarker(point);
+	map.addOverlay(marker);
+	if (text)
+	{
+		marker.openInfoWindowHtml(text);
+	}
 }
 
 function showAddress(id, address)
 {
-	initMap(id)
-    if (geocoder) 
-    {
-        geocoder.getLatLng(address,
-            function(point) 
-            {
-                if (point) 
-                {
-                    map.setCenter(point, 13);
-                    var marker = new GMarker(point);
-                    map.addOverlay(marker);
-                    marker.openInfoWindowHtml(address);
-                }
-            }
-        );
-    }
+	
+	if (!geocoder)
+	{
+		geocoder = new GClientGeocoder();
+	}
+	
+	geocoder.getLatLng(address, function(point)
+	{
+		showPoint(id, point, address);
+	});
+	
 }
