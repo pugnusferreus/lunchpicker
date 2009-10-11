26a5a974be8cda558c2c111637b46b21c36c0a56
Benson Lim <benson@spawncamp.net>
2009-10-11 19:24:13 +0800

    I am a n00b

--- db/migrate/20091011105958_venues.rb
+++ db/migrate/20091011105958_venues.rb
@@ -1,7 +1,16 @@
 class Venues < ActiveRecord::Migration
   def self.up
+    create_table :venues do |t|
+      t.string :name
+      t.string :address
+      t.text :comments
+      t.references :location
+      t.boolean :sheltered, :default => false
+      t.timestamps
+    end
   end
 
   def self.down
+    drop_table :venues
   end
 end
