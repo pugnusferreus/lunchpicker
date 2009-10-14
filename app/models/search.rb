9111ed63542faf5b2b5f5774def3af75adf73ee2
Benson Lim <benson@spawncamp.net>
2009-10-14 10:23:18 +0800

    added weather info in the search view

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,29 +3,31 @@ class Search
   
   validates_presence_of :location_id
 
-  attr_accessor :location_id, :location_name, :venue_name, :venue_address, :comments, :sheltered
+  attr_accessor :location_id, :location_name, :venue_name, :venue_address, :comments, :sheltered, :weather_condition, :weather_temp_c, :weather_wind_condition, :weather_humidity
   
   def find_venue(loc_id,check_weather)
      @location_id = loc_id
     
     #check if validation pass
     if valid?
-    
-    
+      location = Location.find(loc_id)
+      google_weather = GoogleWeather.new(location.weather_location)
+      has_data = google_weather.weather
+
       if(check_weather.nil? || check_weather == "false") then
         @venue = Venue.first :order => 'random()', :conditions => ["location_id = ?", @location_id]
       else
-        location = Location.find(loc_id)
-        
-        google_weather = GoogleWeather.new(location.weather_location)
         
         needs_shelter = false
-        $NEED_SHELTER_WEATHERS.each do |condition|
-          if (condition.casecmp(google_weather.current_conditions.condition) == 0) then
-            needs_shelter = true
+        if(has_data.key? 'current_conditions') then
+          $NEED_SHELTER_WEATHERS.each do |condition|
+            if (condition.casecmp(google_weather.current_conditions.condition) == 0) then
+              needs_shelter = true
+            end
           end
-        end
-        
+
+        end    
+   
         if(!needs_shelter) then
           @venue = Venue.first :order => 'random()', :conditions => ["location_id = ?", @location_id]
         else
@@ -40,6 +42,15 @@ class Search
         @venue_address = @venue.address
         @comments = @venue.comments
         @sheltered = check_weather
+
+
+        if(has_data.key? 'current_conditions') then
+          @weather_condition = google_weather.current_conditions.condition
+          @weather_temp_c = google_weather.current_conditions.temp_c
+          @weather_wind_condition = google_weather.current_conditions.wind_condition
+          @weather_humidity = google_weather.current_conditions.humidity
+
+        end
       end
       
     end
