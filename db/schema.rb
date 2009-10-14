61b4dda03c2aa50b6b03a022348354838e2bd207
Benson Lim <benson@spawncamp.net>
2009-10-14 15:26:41 +0800

    added user table

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,31 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090928090404) do
+ActiveRecord::Schema.define(:version => 20091014035735) do
 
   create_table "locations", :primary_key => "location_id", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "weather_location", :limit => 100
+    t.string   "weather_location"
+  end
+
+  create_table "users", :primary_key => "user_id", :force => true do |t|
+    t.string   "login",                              :null => false
+    t.string   "crypted_password",                   :null => false
+    t.string   "password_salt",                      :null => false
+    t.string   "persistence_token",                  :null => false
+    t.string   "single_access_token",                :null => false
+    t.string   "perishable_token",                   :null => false
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "venues", :primary_key => "venue_id", :force => true do |t|
@@ -25,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20090928090404) do
     t.integer  "location_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "sheltered",   :default => false
   end
 
 end
