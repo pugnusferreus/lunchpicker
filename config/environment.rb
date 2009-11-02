001b79bd930b0533cb6ce73598d6007302b4b407
Benson Lim <benson@spawncamp.net>
2009-11-02 18:40:16 +0800

    new google map key

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,8 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 #Google maps key
-GOOGLE_MAP_KEY = 'ABQIAAAAD3guJbG5cT8WfO7vt9ClXhT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSQHgp1aR-x-tXoC8woO7zH5yBT-g' #for localhost
-
+#GOOGLE_MAP_KEY = 'ABQIAAAAD3guJbG5cT8WfO7vt9ClXhT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSQHgp1aR-x-tXoC8woO7zH5yBT-g' #for localhost
+GOOGLE_MAP_KEY = 'ABQIAAAAD3guJbG5cT8WfO7vt9ClXhSIcLphRYVPagdrHN6Zvi0KzoRl1hRfmE4isMNdNN2UF_kQoZQCE8gJ8w'
 #all possible values from google weather that needs to go to a place with shelter
 NEED_SHELTER_WEATHERS = Array["storm","thunderstorm","snow","rain"]
 
