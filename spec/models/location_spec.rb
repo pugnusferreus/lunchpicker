5ae089a6b29102f775077f8c272cbee875c66d2e
Benson Lim <benson@progriff.com>
2011-07-02 17:02:01 +1000

    added CRUD test for location

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -7,4 +7,26 @@ describe Location do
     location = Location.find(location.location_id)
     location.name.should eql "Location 1"
   end
+
+  it 'should update location' do
+    location = Location.create! :name => "Location 1", :weather_location => "Braybrook, Victoria"
+
+    location = Location.find(location.location_id)
+    location.name.should eql "Location 1"
+    location.name = "Location 1 new name"
+    location.save!
+
+    location = Location.find(location.location_id)
+    location.name.should eql "Location 1 new name"
+  end
+
+  it 'should delete location' do
+    location = Location.create! :name => "Location 1", :weather_location => "Braybrook, Victoria"
+
+    location = Location.find(location.location_id)
+    location_id = location.location_id
+    location.destroy
+    lambda{ Location.find(location_id) }.should raise_error
+  end 
+
 end
