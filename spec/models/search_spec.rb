6f3ce8450eeeb14459b21d998f4d0e4d0bfa5f35
Benson Lim <benson@progriff.com>
2011-07-02 14:09:27 +1000

    added test for search_spec

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -2,12 +2,35 @@ require 'spec_helper'
 
 describe Search do
   before(:each) do
-    @location = Location.new(:name => "Location 1", :weather_location => "Braybrook, Victoria")
-    @location.save
+    @braybrook = Location.new(:name => "Braybrook", :weather_location => "Braybrook, Victoria")
+    @braybrook.save
+
+    @braybrook_venue1 = Venue.new(:name => "Braybrook Venue 1", :address => "blah", :comments => "some comments", :location => @braybrook, :sheltered => false)
+    @braybrook_venue1.save
+
+    @braybrook_venue2 = Venue.new(:name => "Braybrook Venue 2", :address => "blah", :comments => "some comments", :location => @braybrook, :sheltered => true)
+    @braybrook_venue2.save
+
+    @sunshine = Location.new(:name => "Sunshine", :weather_location => "Sunshine, Victoria")
+    @sunshine.save
+    
   end
 
-  it 'should have tests' do
-    p Location.all
-    pending
+  it 'should have have venues' do
+    p Venue.all
+    @search = Search.new
+    @search.find_venue :location_id => @braybrook.location_id, :sheltered => false
+    venue = @search.venue
+    p venue
+    venue.should_not be_nil
+    venue.location.name.should eq "Braybrook"
   end
+
+  #it 'should have no venues' do
+  #  search = Search.new
+  #  search.find_venue :location_id => @sunshine.location_id, :sheltered => false
+  #  venue = search.venue
+  #  p venue
+  #  venue.should be nil
+  #end
 end
