fb7f7705af8f1dbf844e0a3721e8d7448a853d7a
Benson Lim <benson@progriff.com>
2011-07-02 23:25:22 +1000

    test for locations_controller

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -3,11 +3,16 @@ require 'spec_helper'
 describe LocationsController do
   before(:each) do
     activate_authlogic
-    user = User.create! :login=>"pugnusferreus"
+    user = User.create! :login=>"pugnusferreus", :password=>"foobar", :password_confirmation=>"foobar"
     UserSession.create user
+
+    #@location = Location.create! :name => "Location 1", :weather_location => "Braybrook, Victoria"
+
   end
 
   it 'should show a list of locations' do
-    p Location.all
+    get :index
+    should respond_with(:success)
+    #response.should render_template("path/to/template/for/action")
   end
 end
