074909d2eeded985d0c293805f75161167cca5b6
Benson Lim <benson@progriff.com>
2011-07-03 21:12:08 +1000

    added more specs for location controller

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -13,6 +13,43 @@ describe LocationsController do
   it 'should show a list of locations' do
     get :index
     should respond_with(:success)
-    #response.should render_template("path/to/template/for/action")
+  end
+
+  it 'should show new location' do
+    get :new
+    should respond_with(:success)
+  end
+
+  it 'should show a location by id' do
+    location = Location.create! :name => "Location 1", :weather_location => "Braybrook, Victoria"
+    get :show, :id=>location.id
+    should respond_with(:success)
+  end
+
+  it 'should show edit location' do
+    location = Location.create! :name => "Location 1", :weather_location => "Braybrook, Victoria"
+    get :edit, :id=>location.id
+    should respond_with(:success)
+  end
+
+  it 'should create a location' do
+    post :create, :location=>{:name=>"test create",:weather_location =>"Braybrook, Victoria"}
+    location = Location.where(:name=>"test create").first
+    location.name.should eql "test create"
+    response.should redirect_to(location)
+  end
+
+  it 'should update a location' do
+    location = Location.create! :name => "test update", :weather_location => "Braybrook, Victoria"
+    post :update, :id=>location.id,:location=>{:name=>"New test update",:weather_location=>"Braybrook, Victoria"}
+    location = Location.where(:name=>"New test update").first
+    location.name.should eql "New test update"
+    response.should redirect_to(location)
+  end
+
+  it 'should destroy a location' do
+    location = Location.create! :name => "test delete", :weather_location => "Braybrook, Victoria"
+    get :destroy, :id=>location.id
+    response.should redirect_to(Location)
   end
 end
