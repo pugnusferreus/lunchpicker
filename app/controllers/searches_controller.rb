pugnusferreus/lunchpicker

commit d6c95f99745067790410e1e3aacffcae6828d785

forgot to commit Gemfile.lock

app/controllers/searches_controller.rb
@@ -1,4 +1,6 @@
 class SearchesController < ApplicationController
+  layout 'default'
+  
   def index
     @search = Search.new
     @search.find_venue(params[:search]) if request.post?

commit 3256bdca2c894b9c9419a09a2737b017591d0c20

Use default layout in search controller.
Fixes regression introduced in d6c95f99745067790410e1e3aacffcae6828d785.

app/controllers/searches_controller.rb
@@ -1,6 +1,4 @@
 class SearchesController < ApplicationController
-  layout 'default'
-
   def index
     @search = Search.new
     @search.find_venue(params[:search]) if request.post?